beff926a25b35f54a1887dbc7e3473cdf4601a1f
Tobias Malmsheimer <malmsheimer@hdm-stuttgart.de>
2024-03-13 11:50:59 +0100

    update index generation for better usability

--- labels_for_elastic_search.py
+++ labels_for_elastic_search.py
@@ -1,84 +1,89 @@
+import argparse
 import elasticsearch
 from elasticsearch.helpers import bulk
 from SPARQLWrapper import SPARQLWrapper, JSONLD
-from rdflib import URIRef
+from rdflib.term import URIRef
 from tqdm import tqdm
 
-sparql = SPARQLWrapper("http://localhost:3030/public")
+parser = argparse.ArgumentParser(description="Indexes all uris which have a rdf#type and rdfs#label.")
+
+parser.add_argument("--sparql", default="http://localhost:3030/jvmg/sparql", help="Address to sparql endpoint (default: http://localhost:3030/jvmg/sparql)")
+parser.add_argument("--elasticsearch", default="http://localhost:9200", help="Address to elasticsearch (default: http://localhost:9200)")
+parser.add_argument("--index", required=True, help="Index name for elasticsearch.")
+args = parser.parse_args()
+
+sparql = SPARQLWrapper(args.sparql)
 sparql.setReturnFormat(JSONLD)
-index_name = "default"
 
+es = elasticsearch.Elasticsearch(args.elasticsearch)
+es.options(ignore_status=[400, 404]).indices.delete(index=args.index)
 
-es = elasticsearch.Elasticsearch("http://localhost:9200")
-es.indices.delete(index=index_name, ignore=[400, 404])
-settings =  {
-        "number_of_shards": 6,
-        "number_of_replicas": 1
+mappings = {
+    'properties': {
+        'uri':   {'type': 'text'},
+        'label': {'type': 'text'},
+        'graph': {'type': 'keyword'},
+        'type':  {'type': 'keyword'}
     }
-mappings = {'properties': {
-        'uri': {'type': 'keyword'},
-        'label': {'type': 'wildcard'},
-        'graph': {'type': 'wildcard'},
-        'type': {'type': 'wildcard'}}}
+}
 
-es.indices.create(index=index_name, ignore=400, settings=settings, mappings=mappings)
+es.options(ignore_status=400).indices.create(index=args.index, mappings=mappings)
+
 
 query = """
-prefix label: <http://www.w3.org/2000/01/rdf-schema#label>
-prefix type: <http://www.w3.org/1999/02/22-rdf-syntax-ns#type>
-prefix graph_label: <http://mediagraph.link/jvmg/ont/shortLabel>
-construct {
-  graph ?graph_uri {
-    ?uri label: ?label .
-    ?uri type: ?type_uri .
-    ?graph_uri graph_label: ?graph .
-    ?type_uri label: ?type
-  }
-} WHERE {
-  graph ?graph_uri {
-    ?uri label: ?label .
-    ?uri type: ?type_uri
-    optional {
-      ?graph_uri graph_label: ?graph
+    CONSTRUCT {
+       ?uri <http://www.w3.org/2000/01/rdf-schema#label> ?label .
+       ?uri <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> ?type .
+       ?type <http://www.w3.org/2000/01/rdf-schema#label> ?type_label .
+       ?uri <http://mediagraph.link/jvmg/ont/hasGraph> ?graph .
+       ?graph <http://mediagraph.link/jvmg/ont/shortLabel> ?graph_label
+    } WHERE {
+    graph ?graph{
+      ?uri <http://www.w3.org/2000/01/rdf-schema#label> ?label .
     }
-    optional {
-        ?type_uri label: ?type
+    ?uri <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> ?type .
+    OPTIONAL {
+      ?type <http://www.w3.org/2000/01/rdf-schema#label> ?type_label .
     }
-  }
-}"""
+    ?graph <http://mediagraph.link/jvmg/ont/shortLabel> ?graph_label
+    }"""
 
+print("executing sparql query...")
 sparql.setQuery(query)
-sparql_data = sparql.queryAndConvert()
-
-uri_query = """
-prefix label: <http://www.w3.org/2000/01/rdf-schema#label>
-prefix type: <http://www.w3.org/1999/02/22-rdf-syntax-ns#type>
-select distinct ?uri ?graph
-where {
-  graph ?graph{
-    ?uri label: ?label .
-    ?uri type: ?type_uri
-  }
-}
-"""
+sparql_data = sparql.query().convert()
 
 LABEL = URIRef("http://www.w3.org/2000/01/rdf-schema#label")
 TYPE = URIRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#type")
+GRAPH = URIRef("http://mediagraph.link/jvmg/ont/hasGraph")
 GRAPH_LABEL = URIRef("http://mediagraph.link/jvmg/ont/shortLabel")
 
-uris = list(sparql_data.query(uri_query))
-
 def gen_docs():
-    for uri, graph in uris:
-        labels = [str(item) for item in sparql_data.objects(subject=uri, predicate=LABEL)]
-        types = [str(item) for item in sparql_data.objects(subject=uri, predicate=TYPE)]
-        graph_labels = [str(item) for item in sparql_data.objects(subject=graph, predicate=GRAPH_LABEL)]
+    for uri in tqdm(set(sparql_data.subjects(predicate=TYPE))):
+        labels  = [str(label) for label in sparql_data.objects(subject=uri, predicate=LABEL)]
+        type_labels = []
+        graph_labels = []
+
+        for type in set(sparql_data.objects(subject=uri, predicate=TYPE)):
+            new_labels = [str(label) for label in sparql_data.objects(subject=type, predicate=LABEL)]
+            if len(new_labels):
+                type_labels.extend(new_labels)
+            else:
+                type_labels.append(str(type))
+
+        for graph in set(sparql_data.objects(subject=uri, predicate=GRAPH)):
+            new_labels = [str(label) for label in sparql_data.objects(subject=graph, predicate=GRAPH_LABEL)]
+            if len(new_labels):
+                graph_labels.extend(new_labels)
+            else:
+                graph_labels.append(str(graph))
+
         doc = {
             "uri": str(uri),
             "label": labels,
-            "type": types,
-            "graph": graph_labels
+            "type": type_labels,
+            "graph": graph_labels,
         }
-        yield {"_index": index_name, "_source": doc}
+
+        yield {"_index": args.index, "_source": doc}
 
 bulk(es, gen_docs())
